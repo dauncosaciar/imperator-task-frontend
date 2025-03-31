7ea6293007192326cb8d0ca30b070ac6b39dedd4
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-03-31 18:27:53 -0300

    create app router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Router from "./router";
 import "./styles/app.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1 className="text-center">ImperatorTask</h1>
+    <Router />
   </StrictMode>
 );
