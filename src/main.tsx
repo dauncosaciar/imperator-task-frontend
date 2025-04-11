dfb69277b9209c6f6c810c2ca03b89a3cb0b6119
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-04-11 11:13:58 -0300

    install react-query dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/chakra-ui/provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider as ChakraProvider } from "@/components/chakra-ui/provider";
 import Router from "./router";
 import "./styles/main.scss";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <Router />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Router />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
