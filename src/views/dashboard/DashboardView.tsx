8f1bf3ea0ecfc145e0dfcffea6d47049b6a82369
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-04-14 15:55:59 -0300

    get projects using use-query from react-query

--- src/views/dashboard/DashboardView.tsx
+++ src/views/dashboard/DashboardView.tsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import { FolderPlus } from "lucide-react";
+import { getProjects } from "@/api/ProjectApi";
 
 export default function DashboardView() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["projects"],
+    queryFn: getProjects
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+
   return (
     <div className="dashboard-view">
       <h1 className="dashboard-view__heading">Mis Proyectos</h1>
