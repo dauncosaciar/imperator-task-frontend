1149d9835cd68dec1954b20b3a0126d8fb70f651
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-04-25 12:59:41 -0300

    install react-query-devtools dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider as ChakraProvider } from "@/components/chakra-ui/provider";
 import Router from "./router";
 import "./styles/main.scss";
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <Router />
+        <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
   </StrictMode>
