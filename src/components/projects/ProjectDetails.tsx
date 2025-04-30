26f816d6dd5941940cdba07e0fcc532a0c2c87d9
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-04-30 11:59:29 -0300

    add first details to ProjectDetailsView

---
+++ src/components/projects/ProjectDetails.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { ListFilterPlus } from "lucide-react";
+
+export default function ProjectDetails({ data }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="project-details">
+      <h1 className="project-details__heading">Proyecto: {data.projectName}</h1>
+      <p className="project-details__description">{data.description}</p>
+
+      <nav className="project-details__nav">
+        <button
+          type="button"
+          className="project-details__nav-link"
+          onClick={() => navigate("?newTask=true")}
+        >
+          <ListFilterPlus /> Agregar Tarea
+        </button>
+      </nav>
+    </div>
+  );
+}
--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,3 +1,32 @@
+import { useEffect } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectApi";
+import Spinner from "@/components/ui/Spinner";
+import { changeDocumentTitle } from "@/utils";
+import ProjectDetails from "@/components/projects/ProjectDetails";
+
 export default function ProjectDetailsView() {
-  return <div>ProjectDetailsView</div>;
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const { data, isFetching, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+    refetchOnWindowFocus: false,
+    retry: false
+  });
+
+  useEffect(() => {
+    if (data) {
+      const documentTitle = `Detalles del Proyecto: ${data.projectName}`;
+      changeDocumentTitle(documentTitle);
+    }
+  }, [data]);
+
+  if (isFetching) return <Spinner />;
+
+  if (isError) return <Navigate to="/404" />;
+
+  if (data) return <ProjectDetails data={data} />;
 }
