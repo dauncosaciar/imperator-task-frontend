9edf9ce1c2126f3770d821ea5433a642d91ffcf9
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-05-05 14:15:40 -0300

    add initial styles to tasks list and cards

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,3 +1,4 @@
+import { EllipsisVertical } from "lucide-react";
 import { Task } from "@/types";
 
 type TaskCardProps = {
@@ -7,7 +8,14 @@ type TaskCardProps = {
 export default function TaskCard({ task }: TaskCardProps) {
   return (
     <li className="task-card">
-      <p>{task.name}</p>
+      <div className="task-card__content">
+        <h4 className="task-card__name">{task.name}</h4>
+        <p className="task-card__description">{task.description}</p>
+      </div>
+
+      <div className="task-card__menu">
+        <EllipsisVertical />
+      </div>
     </li>
   );
 }
