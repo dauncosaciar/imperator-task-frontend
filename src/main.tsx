98bcda5aa6a51a86859334eb7dde0689d3f76798
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-05-05 11:42:52 -0300

    add light mode to the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider as ChakraProvider } from "@/components/chakra-ui/provider";
+import { LightMode } from "@/components/chakra-ui/color-mode";
 import Router from "./router";
 import "./styles/main.scss";
 
@@ -12,8 +13,10 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <Router />
-        <ReactQueryDevtools />
+        <LightMode>
+          <Router />
+          <ReactQueryDevtools />
+        </LightMode>
       </ChakraProvider>
     </QueryClientProvider>
   </StrictMode>
