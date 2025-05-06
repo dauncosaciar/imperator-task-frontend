5f4c2d54f2efce97ba016068bd039dfb89425b35
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-05-06 15:01:16 -0300

    add menu mixin

--- src/components/ui/AvatarMenu.tsx
+++ src/components/ui/AvatarMenu.tsx
@@ -10,27 +10,25 @@ export default function AvatarMenu() {
         </div>
       </Menu.Trigger>
       <Portal>
-        <Menu.Positioner>
-          <Menu.Content>
-            <div className="avatar-menu">
-              <div className="avatar-menu__info">
-                <div className="avatar-menu__image">
-                  <CircleUser color="#0f6130" />
-                </div>
-                <div className="avatar-menu__user">
-                  <p className="avatar-menu__user-name">Nombre</p>
-                  <p className="avatar-menu__user-email">email@email.com</p>
-                </div>
+        <Menu.Positioner className="avatar-menu">
+          <Menu.Content className="avatar-menu__content">
+            <div className="avatar-menu__info">
+              <div className="avatar-menu__image">
+                <CircleUser color="#0f6130" />
               </div>
-              <nav className="avatar-menu__nav">
-                <a className="avatar-menu__nav-link" href="#">
-                  Mi Perfil
-                </a>
-                <a className="avatar-menu__nav-link" href="#">
-                  Cerrar Sesión
-                </a>
-              </nav>
+              <div className="avatar-menu__user">
+                <p className="avatar-menu__user-name">Nombre</p>
+                <p className="avatar-menu__user-email">email@email.com</p>
+              </div>
             </div>
+            <nav className="avatar-menu__nav">
+              <a className="avatar-menu__nav-link" href="#">
+                Mi Perfil
+              </a>
+              <a className="avatar-menu__nav-link" href="#">
+                Cerrar Sesión
+              </a>
+            </nav>
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
