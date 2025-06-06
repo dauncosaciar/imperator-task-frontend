ad6c927ad844a26f576ac5f58e2f77464b250a62
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-06-06 17:00:21 -0300

    fix TaskCardMenu closing when clicking on any of its options

--- src/components/tasks/TaskCardMenu.tsx
+++ src/components/tasks/TaskCardMenu.tsx
@@ -20,24 +20,32 @@ export default function TaskCardMenu({ task }: TaskCardMenuProps) {
       <Portal>
         <Menu.Positioner className="task-card-menu">
           <Menu.Content className="task-card-menu__content">
-            <button type="button" className="task-card-menu__button">
-              Ver Tarea
-            </button>
-            <button
-              type="button"
-              className="task-card-menu__button"
-              onClick={() =>
-                navigate(location.pathname + `?editTask=${task._id}`)
-              }
-            >
-              Editar Tarea
-            </button>
-            <button
-              type="button"
-              className="task-card-menu__button task-card-menu__button--delete"
-            >
-              Eliminar Tarea
-            </button>
+            <Menu.Item className="task-card-menu__item" value="Ver Tarea">
+              <button type="button" className="task-card-menu__button">
+                Ver Tarea
+              </button>
+            </Menu.Item>
+
+            <Menu.Item className="task-card-menu__item" value="Editar Tarea">
+              <button
+                type="button"
+                className="task-card-menu__button"
+                onClick={() =>
+                  navigate(location.pathname + `?editTask=${task._id}`)
+                }
+              >
+                Editar Tarea
+              </button>
+            </Menu.Item>
+
+            <Menu.Item className="task-card-menu__item" value="Eliminar Tarea">
+              <button
+                type="button"
+                className="task-card-menu__button task-card-menu__button--delete"
+              >
+                Eliminar Tarea
+              </button>
+            </Menu.Item>
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
