0c55a107cebef9c25128fd5b8b197c158ff0c43b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-06-25 17:02:58 -0300

    add AuthLayout css code

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,22 +1,39 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useBreakpointValue } from "@chakra-ui/react";
 import Logo from "@/components/ui/Logo";
 
 export default function AuthLayout() {
+  const [mounted, setMounted] = useState(false);
+  const isMobile = useBreakpointValue({ base: true, lg: false })!;
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="auth-layout">
       <div className="auth-layout__grid">
         <div className="auth-layout__content">
-          <Logo />
-          <Outlet />
+          <div className="auth-layout__frame">
+            <Logo />
+            <Outlet />
+          </div>
         </div>
 
-        <div className="auth-layout__hero">
-          <h1 className="auth-layout__title">Bienvenido a ImperatorTask</h1>
-          <p className="auth-layout__text">
-            ImperatorTask te ayuda a llevar adelante proyectos y tareas de forma
-            organizada.
-          </p>
-        </div>
+        {!isMobile && (
+          <div className="auth-layout__hero">
+            <div className="auth-layout__welcome">
+              <h1 className="auth-layout__title">Bienvenido a ImperatorTask</h1>
+              <p className="auth-layout__text">
+                El dashboard que te ayuda a llevar adelante proyectos y tareas
+                de forma organizada.
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
