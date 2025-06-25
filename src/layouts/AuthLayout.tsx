3af4b0dd3394b1a55362dde12aa1941c8dddddda
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-06-25 14:49:30 -0300

    add AuthLayout html code

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,23 @@
 import { Outlet } from "react-router-dom";
+import Logo from "@/components/ui/Logo";
 
 export default function AuthLayout() {
   return (
-    <>
-      <div>AuthLayout</div>
-      <Outlet />
-    </>
+    <div className="auth-layout">
+      <div className="auth-layout__grid">
+        <div className="auth-layout__content">
+          <Logo />
+          <Outlet />
+        </div>
+
+        <div className="auth-layout__hero">
+          <h1 className="auth-layout__title">Bienvenido a ImperatorTask</h1>
+          <p className="auth-layout__text">
+            ImperatorTask te ayuda a llevar adelante proyectos y tareas de forma
+            organizada.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
