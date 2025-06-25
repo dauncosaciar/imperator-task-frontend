37cabcebe856511a749361245abc58cb8ee97360
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-06-25 17:19:34 -0300

    change document title of LoginView

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+import { changeDocumentTitle } from "@/utils";
+
 export default function LoginView() {
+  useEffect(() => {
+    changeDocumentTitle("Inicia sesión");
+  }, []);
+
   return <div>LoginView</div>;
 }
