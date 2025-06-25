46cf9ec15d2a1b0e8ab24b0fdd70342fa84617aa
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-06-25 20:03:35 -0300

    add LoginForm

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,63 @@
+import ErrorMessage from "../form/ErrorMessage";
+
+type LoginFormProps = {
+  register: UseFormRegister<LoginFormData>;
+  errors: FieldErrors<LoginFormData>;
+  mutationExecuting: boolean;
+};
+
+export default function LoginForm({
+  register,
+  errors,
+  mutationExecuting
+}: LoginFormProps) {
+  return (
+    <>
+      <div className="form__field">
+        <label htmlFor="email" className="form__label">
+          Email
+        </label>
+        <input
+          id="email"
+          className={`form__input${errors.email ? " form__input--error" : ""}${
+            mutationExecuting ? " form__input--disabled" : ""
+          }`}
+          type="email"
+          placeholder="mi.correo@ejemplo.com"
+          disabled={mutationExecuting}
+          {...register("email", {
+            required: "El Email es obligatorio.",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Email no válido"
+            }
+          })}
+        />
+
+        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+      </div>
+
+      <div className="form__field">
+        <label htmlFor="password" className="form__label">
+          Contraseña
+        </label>
+        <input
+          id="password"
+          className={`form__input${
+            errors.password ? " form__input--error" : ""
+          }${mutationExecuting ? " form__input--disabled" : ""}`}
+          type="password"
+          placeholder="Contraseña que elegiste"
+          disabled={mutationExecuting}
+          {...register("password", {
+            required: "La Contraseña es obligatoria."
+          })}
+        />
+
+        {errors.password && (
+          <ErrorMessage>{errors.password.message}</ErrorMessage>
+        )}
+      </div>
+    </>
+  );
+}
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,10 +1,56 @@
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { LogIn } from "lucide-react";
 import { changeDocumentTitle } from "@/utils";
+import Form from "@/components/form/Form";
+import LoginForm from "@/components/auth/LoginForm";
 
 export default function LoginView() {
+  const initialValues: LoginFormData = {
+    email: "",
+    password: ""
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ defaultValues: initialValues });
+
   useEffect(() => {
     changeDocumentTitle("Inicia sesión");
   }, []);
 
-  return <div>LoginView</div>;
+  const handleForm = () => {
+    console.log("iniciando sesión...");
+  };
+
+  return (
+    <div className="login-view">
+      <h2 className="login-view__heading">Inicia sesión</h2>
+      <p className="login-view__text">Para gestionar tus proyectos y tareas.</p>
+
+      <div className="login-view__content">
+        <Form
+          handleSubmit={handleSubmit}
+          fnSubmit={handleForm}
+          InnerForm={LoginForm}
+          register={register}
+          errors={errors}
+          // mutationExecuting={isPending}
+          mutationExecuting={false}
+          spinnerMessage="Creando proyecto"
+          submitIcon={LogIn}
+          submitText="Iniciar Sesión"
+        />
+
+        <div className="login-view__question">
+          <p className="login-view__question-text">¿Nuevo en ImperatorTask?</p>
+          <a href="#" className="login-view__question-link">
+            Regístrate
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
