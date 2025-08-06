f5187c970bb7c17596ddd1b6713dd1ea42bb532e
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-08-06 15:18:05 -0300

    update AvatarMenu component to show name, lastName and email of logged-in user

--- src/components/ui/AvatarMenu.tsx
+++ src/components/ui/AvatarMenu.tsx
@@ -1,37 +1,44 @@
 import { CircleUser } from "lucide-react";
 import { Menu, Portal } from "@chakra-ui/react";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function AvatarMenu() {
-  return (
-    <Menu.Root>
-      <Menu.Trigger asChild>
-        <div className="app-avatar">
-          <CircleUser color="#0f6130" />
-        </div>
-      </Menu.Trigger>
-      <Portal>
-        <Menu.Positioner className="avatar-menu">
-          <Menu.Content className="avatar-menu__content">
-            <div className="avatar-menu__info">
-              <div className="avatar-menu__image">
-                <CircleUser color="#0f6130" />
+  const { data: user } = useAuth();
+
+  if (user)
+    return (
+      <Menu.Root>
+        <Menu.Trigger asChild>
+          <div className="app-avatar">
+            <CircleUser color="#0f6130" />
+          </div>
+        </Menu.Trigger>
+        <Portal>
+          <Menu.Positioner className="avatar-menu">
+            <Menu.Content className="avatar-menu__content">
+              <div className="avatar-menu__info">
+                <div className="avatar-menu__image">
+                  <CircleUser color="#0f6130" />
+                </div>
+                <div className="avatar-menu__user">
+                  <p className="avatar-menu__user-name">
+                    {user.name}, {user.lastName}
+                  </p>
+                  <p className="avatar-menu__user-email">{user.email}</p>
+                </div>
               </div>
-              <div className="avatar-menu__user">
-                <p className="avatar-menu__user-name">Nombre</p>
-                <p className="avatar-menu__user-email">email@email.com</p>
-              </div>
-            </div>
-            <nav className="avatar-menu__nav">
-              <a className="avatar-menu__nav-link" href="#">
-                Mi Perfil
-              </a>
-              <a className="avatar-menu__nav-link" href="#">
-                Cerrar Sesión
-              </a>
-            </nav>
-          </Menu.Content>
-        </Menu.Positioner>
-      </Portal>
-    </Menu.Root>
-  );
+
+              <nav className="avatar-menu__nav">
+                <a className="avatar-menu__nav-link" href="#">
+                  Mi Perfil
+                </a>
+                <a className="avatar-menu__nav-link" href="#">
+                  Cerrar Sesión
+                </a>
+              </nav>
+            </Menu.Content>
+          </Menu.Positioner>
+        </Portal>
+      </Menu.Root>
+    );
 }
