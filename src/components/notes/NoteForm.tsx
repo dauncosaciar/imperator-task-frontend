a515524bcf53512e28feada831a1a98a99b18489
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-10-17 14:54:18 -0300

    add addNoteForm fields inside NoteForm component

--- src/components/notes/NoteForm.tsx
+++ src/components/notes/NoteForm.tsx
@@ -1,3 +1,29 @@
-export default function NoteForm() {
-  return <div>NoteForm</div>;
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+import ErrorMessage from "../form/ErrorMessage";
+
+export default function NoteForm({ register, errors, mutationExecuting }) {
+  return (
+    <>
+      <div className="form__field">
+        <label htmlFor="content" className="form__label">
+          Nueva nota
+        </label>
+        <textarea
+          id="content"
+          className={`form__input${
+            errors.content ? " form__input--error" : ""
+          }${mutationExecuting ? " form__input--disabled" : ""}`}
+          type="text"
+          placeholder="Lo que desees dejar registrado"
+          disabled={mutationExecuting}
+          {...register("content", {
+            required: "El Contenido de la Nota es obligatorio."
+          })}
+        />
+        {errors.content && (
+          <ErrorMessage>{errors.content.message}</ErrorMessage>
+        )}
+      </div>
+    </>
+  );
 }
