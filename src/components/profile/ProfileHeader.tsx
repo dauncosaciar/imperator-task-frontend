c45548a68b885d8f040d9097d09f665a7f308cde
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-10-22 20:44:36 -0300

    add an image inside a ProfileHeader component and update the ProfileLayout

---
+++ src/components/profile/ProfileHeader.tsx
@@ -0,0 +1,10 @@
+export default function ProfileHeader() {
+  return (
+    <div className="profile-header">
+      <div className="profile-header__card">
+        <div className="profile-header__card-image" />
+        <div className="profile-header__card-info"></div>
+      </div>
+    </div>
+  );
+}
--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import ProfileHeader from "@/components/profile/ProfileHeader";
 
 export default function ProfileLayout() {
   return (
     <div className="profile-layout">
-      <div>ProfileLayout</div>
-      <Outlet />
+      <h1 className="profile-layout__heading">Mi Perfil</h1>
+
+      <div className="profile-layout__content">
+        <ProfileHeader />
+        <Outlet />
+      </div>
     </div>
   );
 }
