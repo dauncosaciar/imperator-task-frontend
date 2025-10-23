223b3273750cecdc6df6394ba788341e2b72510a
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-10-23 12:51:48 -0300

    show hardcoded user info in UserInitials component and in a heading inside ProfileHeader

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,9 +1,31 @@
+import UserInitials from "../ui/UserInitials";
+
 export default function ProfileHeader() {
+  // TODO: hardcoded user, replace with dynamic info later
+  const user = {
+    _id: "68869fa7a1192a9e62f1360c",
+    email: "isa.dina@roma.com",
+    lastName: "Dina",
+    name: "Isa"
+  };
+
   return (
     <div className="profile-header">
       <div className="profile-header__card">
         <div className="profile-header__card-image" />
-        <div className="profile-header__card-info"></div>
+
+        <div className="profile-header__card-info">
+          <div className="profile-header__card-user">
+            <UserInitials user={user} />
+
+            <h4 className="profile-header__card-user-name">
+              {user.name}, {user.lastName}
+              <span className="profile-header__card-user-email">
+                {user.email}
+              </span>
+            </h4>
+          </div>
+        </div>
       </div>
     </div>
   );
