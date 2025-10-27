e74a7fe472b301925d534b55313f4cc98df7a35b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-10-27 10:51:42 -0300

    change document title of ProfileView and ChangePasswordView

--- src/views/profile/ChangePasswordView.tsx
+++ src/views/profile/ChangePasswordView.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { changeDocumentTitle } from "@/utils";
+
 export default function ChangePasswordView() {
+  useEffect(() => {
+    const documentTitle = "Cambiar Contraseña";
+    changeDocumentTitle(documentTitle);
+  }, []);
+
   return <div>ChangePasswordView</div>;
 }
--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { changeDocumentTitle } from "@/utils";
+
 export default function ProfileView() {
+  useEffect(() => {
+    const documentTitle = "Mi Cuenta";
+    changeDocumentTitle(documentTitle);
+  }, []);
+
   return <div>ProfileView</div>;
 }
