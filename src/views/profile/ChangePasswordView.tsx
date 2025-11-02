89941a0270fb1061f0784f9990c3376f0528e52d
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-11-02 20:33:58 -0300

    add main texts and styles in ChangePasswordView

--- src/views/profile/ChangePasswordView.tsx
+++ src/views/profile/ChangePasswordView.tsx
@@ -3,9 +3,18 @@ import { changeDocumentTitle } from "@/utils";
 
 export default function ChangePasswordView() {
   useEffect(() => {
-    const documentTitle = "Cambiar Contraseña";
+    const documentTitle = "Cambiar tu Contraseña";
     changeDocumentTitle(documentTitle);
   }, []);
 
-  return <div>ChangePasswordView</div>;
+  return (
+    <div className="change-password-view">
+      <h2 className="change-password-view__heading">Cambia tu Contraseña</h2>
+      <p className="change-password-view__text">
+        Actualiza tu contraseña si lo necesitas.
+      </p>
+
+      <div className="change-password-view__content"></div>
+    </div>
+  );
 }
