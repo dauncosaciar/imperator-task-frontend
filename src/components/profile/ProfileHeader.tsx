1c0d921d33cba22cd295c4a0ec3d6d102213c2ae
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-11-02 17:27:31 -0300

    show user dynamic info in ProfileHeader

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,32 +1,31 @@
+import { useAuth } from "@/hooks/useAuth";
 import UserInitials from "../ui/UserInitials";
+import Spinner from "../ui/Spinner";
 
 export default function ProfileHeader() {
-  // TODO: hardcoded user, replace with dynamic info later
-  const user = {
-    _id: "68869fa7a1192a9e62f1360c",
-    email: "isa.dina@roma.com",
-    lastName: "Dina",
-    name: "Isa"
-  };
+  const { data, isLoading } = useAuth();
 
-  return (
-    <div className="profile-header">
-      <div className="profile-header__card">
-        <div className="profile-header__card-image" />
+  if (isLoading) return <Spinner />;
 
-        <div className="profile-header__card-info">
-          <div className="profile-header__card-user">
-            <UserInitials user={user} />
+  if (data)
+    return (
+      <div className="profile-header">
+        <div className="profile-header__card">
+          <div className="profile-header__card-image" />
 
-            <h4 className="profile-header__card-user-name">
-              {user.name}, {user.lastName}
-              <span className="profile-header__card-user-email">
-                {user.email}
-              </span>
-            </h4>
+          <div className="profile-header__card-info">
+            <div className="profile-header__card-user">
+              <UserInitials user={data} />
+
+              <h4 className="profile-header__card-user-name">
+                {data.name}, {data.lastName}
+                <span className="profile-header__card-user-email">
+                  {data.email}
+                </span>
+              </h4>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
