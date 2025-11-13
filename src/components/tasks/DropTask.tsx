3b221ea3761fbe7bbf391ff726838a720ac144af
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-11-13 18:57:13 -0300

    add a drag style to DropTask component

--- src/components/tasks/DropTask.tsx
+++ src/components/tasks/DropTask.tsx
@@ -7,8 +7,13 @@ type DropTaskProps = {
 export default function DropTask({ status }: DropTaskProps) {
   const { isOver, setNodeRef } = useDroppable({ id: status });
 
+  const style = {
+    border: isOver ? "1px solid #0f6130" : undefined,
+    color: isOver ? "#000" : undefined
+  };
+
   return (
-    <div className="drop-task" ref={setNodeRef}>
+    <div className="drop-task" ref={setNodeRef} style={style}>
       Soltar tarea aquí
     </div>
   );
