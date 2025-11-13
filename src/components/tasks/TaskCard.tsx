317061844c22a1510a7e93e5dbf2436c947d7878
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-11-13 11:33:51 -0300

    use useDraggable hook in TaskCard

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,3 +1,4 @@
+import { useDraggable } from "@dnd-kit/core";
 import TaskCardMenu from "./TaskCardMenu";
 import { Task } from "@/types";
 
@@ -7,9 +8,21 @@ type TaskCardProps = {
 };
 
 export default function TaskCard({ task, canEdit }: TaskCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task._id
+  });
+
+  const style = transform ? {} : undefined;
+
   return (
     <li className="task-card">
-      <div className="task-card__content">
+      <div
+        className="task-card__content"
+        {...listeners}
+        {...attributes}
+        ref={setNodeRef}
+        style={style}
+      >
         <h4 className="task-card__name">{task.name}</h4>
         <p className="task-card__description">{task.description}</p>
       </div>
