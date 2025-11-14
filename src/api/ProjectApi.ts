c60c8b8f08db710ff8498ddad0b8c0c7ec90103e
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-11-14 15:15:13 -0300

    add editProjectSchema and use it in getProjectById method

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,5 +1,10 @@
 import { isAxiosError } from "axios";
-import { dashboardProjectSchema, Project, ProjectFormData } from "@/types";
+import {
+  dashboardProjectSchema,
+  editProjectSchema,
+  Project,
+  ProjectFormData
+} from "@/types";
 import api from "@/lib/axios";
 
 type ProjectApiType = {
@@ -37,7 +42,12 @@ export async function getProjects() {
 export async function getProjectById(projectId: Project["_id"]) {
   try {
     const { data } = await api.get(`/projects/${projectId}`);
-    return data;
+
+    const response = editProjectSchema.safeParse(data);
+
+    if (response.success) {
+      return response.data;
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
