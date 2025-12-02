ad79f8aef0d9731019f025139a6ce264b6542f22
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2025-12-01 21:43:26 -0300

    add NotFoundView content and its styles

--- src/views/404/NotFoundView.tsx
+++ src/views/404/NotFoundView.tsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+import { Undo2, Unlink } from "lucide-react";
+import BasicMessage from "@/components/ui/BasicMessage";
+
 export default function NotFoundView() {
-  return <div className="not-found-view">NotFoundView</div>;
+  return (
+    <div className="not-found-view">
+      <BasicMessage
+        messageIcon={Unlink}
+        messageTitle="¡Ups! Lo sentimos"
+        messageDescription="Parece que la página o recurso que estabas buscando ya no está disponible."
+      />
+
+      <Link className="not-found-view__link" to="/">
+        <Undo2 /> Volver a Mis Proyectos
+      </Link>
+    </div>
+  );
 }
